dc84ca4e9eb686c169e65d0f042e918596d2a6ac
Saharat2022 <saharat59010@gmail.com>
2022-10-05 16:41:13 +0700

    auth Inncepter

--- src/middlewares.js/error.js
+++ src/middlewares.js/error.js
@@ -11,6 +11,8 @@ module.exports = (err, req, res, next) => {
     //คนอ่านจะได้อ่านรู้เรื่อง
     err.message = err.errors[0].message;
   }
-
+  if (err.name === "TokenExpiredError" || err.name === "JsonWebTokenError") {
+    err.statusCode = 401;
+  }
   res.status(err.statusCode || 500).json({ message: err.message });
 };
